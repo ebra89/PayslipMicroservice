ae977c47664612aeed4764265335ad1f2da6aa49
ebrasupertramp <ebra>
2019-06-27 11:40:59 +0200

    initial commit

---
+++ src/main/java/it/gruppoaton/PayslipMicroservice/PayslipMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package it.gruppoaton.PayslipMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayslipMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayslipMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gruppoaton/PayslipMicroservice/PayslipMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package it.gruppoaton.PayslipMicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PayslipMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
