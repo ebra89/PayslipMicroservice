5d21b3bc27742182bc30726842e7782caacd4857
andrea <andrea.tarenzi@gmail.com>
2019-07-01 17:58:52 +0200

    add Watch demon

--- src/main/java/it/gruppoaton/PayslipMicroservice/PayslipMicroserviceApplication.java
+++ src/main/java/it/gruppoaton/PayslipMicroservice/PayslipMicroserviceApplication.java
@@ -1,14 +1,19 @@
 package it.gruppoaton.PayslipMicroservice;
 
+import it.gruppoaton.PayslipMicroservice.Component.WatchService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PayslipMicroserviceApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(PayslipMicroserviceApplication.class, args);
+		//SpringApplication.run(PayslipMicroserviceApplication.class, args);
+		ConfigurableApplicationContext applicationContext=SpringApplication.run(PayslipMicroserviceApplication.class, args);
+		WatchService watchService=(WatchService) applicationContext.getBean("watchService");
+		Thread watchServiceThread=new Thread(watchService, "watch service demon");
 
 	}
 
