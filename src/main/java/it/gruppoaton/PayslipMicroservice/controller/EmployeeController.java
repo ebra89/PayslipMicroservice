8d6b95a3faf1bb766182dceba6dde3cada560666
ebrasupertramp <ebra>
2019-07-04 16:52:48 +0200

    new commit some query in employee controller

--- src/main/java/it/gruppoaton/PayslipMicroservice/controller/EmployeeController.java
+++ src/main/java/it/gruppoaton/PayslipMicroservice/controller/EmployeeController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
     @Autowired
@@ -18,22 +21,18 @@ public class EmployeeController {
     private PayslipService payslipService;
 
 
-    @GetMapping("/employeesList")
+    @GetMapping("/list")
     public String employeeList(Model model){
         model.addAttribute("employees",employeeService.employeeList());
         return "employeeListPage";
     }
 
 
-
-
-
-
-
-
-    @GetMapping("/employees")
+    @GetMapping("/employeesByName")
     public String findByName(Model model, @RequestParam(defaultValue = "") String firstName){
         model.addAttribute("employees",employeeService.findByName(firstName));
         return "employeeListPage";
     }
+
+
 }
--- src/main/java/it/gruppoaton/PayslipMicroservice/repositories/EmployeeRepository.java
+++ src/main/java/it/gruppoaton/PayslipMicroservice/repositories/EmployeeRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
 
-    @Query("select e from Employee e where UPPER (e.firstName) like UPPER (?1)")
+    @Query("select e from Employee e where UPPER (e.firstName) like UPPER (?1) or UPPER (e.lastName) like UPPER (?1) or UPPER (e.fiscalCode) like UPPER (?1)")
     List<Employee> findByNameLike(String firstName);
 
-    @Query("select e from Employee e where e.fiscalCode = ?1")
+    @Query("select e from Employee e where UPPER (e.fiscalCode) = ?1")
     Employee findOneByFC(String fiscalCode);
 
 }
